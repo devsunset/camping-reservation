371bc85ec1fb35694817e8791c469097944aa497
devsunset <devsunset@gmail.com>
2021-04-26 09:13:47 +0900

    dev

--- common/config.py
+++ common/config.py
@@ -25,7 +25,7 @@ HOLYDAY='2021-05-04,2021-05-18,2021-06-05,2021-08-14,2021-09-19,2021-09-20,2021-
 # site  interpark (seperate comma)
 INTERPARK_SITE_NAME='천왕산가족캠핑장,수도권매립지캠핑장,공릉관광지가족캠핑장,평택소풍정원캠핑장'
 INTERPARK_SITE_CODE='20006668,20003504,20008285,20011252'
-INTERPARK_SITE_CHECK_DAY='45,45,5,5'      # Friday (4) , Saturday (5),  Sunday (6)
+INTERPARK_SITE_CHECK_DAY='5,5,5,5'      # Friday (4) , Saturday (5),  Sunday (6)
 INTERPARK_SITE_SEAT_GRADE='1,1:2,1,1:2'
 INTERPARK_SITE_NOT_CHECK_DAY_TIME='10:10,15:14,15:11,15:10'
 INTERPARK_SITE_CHECK_URL='https://api-ticketfront.interpark.com/v1/goods/#INTERPARK_SITE_CODE#/playSeq/PlaySeq/#PLAYSEQ#/REMAINSEAT'
@@ -34,6 +34,6 @@ INTERPARK_SITE_CALENDAR='https://api-ticketfront.interpark.com/v1/goods/#INTERPA
 # site Gwangmyeong
 GWANGMYEONG_SITE_NAME='도덕산캠핑장'
 GWANGMYEONG_SITE_CHECK_URL='https://reserve.gmuc.co.kr/user/camp/campReservation.do?menu=d&menuFlag=C'
-GWANGMYEONG_SITE_CHECK_DAY='45' # Friday (4) , Saturday (5)
+GWANGMYEONG_SITE_CHECK_DAY='5' # Friday (4) , Saturday (5)
 GWANGMYEONG_SITE_SESSION_KEY='A1195C1DEA8DFD2B0E1D4CB1142A20D2.worker2'
 GWANGMYEONG_SITE_SESSION_VALID = 0
