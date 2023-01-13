1ceedf4c689c9a12c97005043e356570b287daec
DEVSUNSET <devsunset@gmail.com>
2023-01-13 15:11:05 +0900

    Update config.py

--- common/config.py
+++ common/config.py
@@ -18,15 +18,15 @@ TELEGRAM_TOKEN = '2020114123:AAG557yWbe1g1e7ZFhZEsO6cn68_9HIozi8'
 TELEGRAM_CHAT_ID =-1001646770464
 
 # day_of_week and holyday
-HOLYDAY='2022-05-05' 
+HOLYDAY='2023-05-05' 
 
 # skip day 
-SKIP_DAY='2022-02-26'
+SKIP_DAY='2023-01-01'
 
 # site  interpark (seperate comma)
 INTERPARK_SITE_NAME='천왕산가족캠핑장,노을진(進)캠핑장,공릉관광지가족캠핑장,한탄강오토캠핑장,경기도청소년수련원캠핑장안산'
 INTERPARK_SITE_CODE='21012652,22011899,20008285,21005592,20003920'
-INTERPARK_SITE_CHECK_DAY='4:5,5,5,5,5'      # Friday (4) , Saturday (5),  Sunday (6)
+INTERPARK_SITE_CHECK_DAY='5,5,5,5,5'      # Friday (4) , Saturday (5),  Sunday (6)
 INTERPARK_SITE_SEAT_GRADE='1,1:2,1,1:2,1'
 INTERPARK_SITE_CHECK_URL='https://api-ticketfront.interpark.com/v1/goods/#INTERPARK_SITE_CODE#/playSeq/PlaySeq/#PLAYSEQ#/REMAINSEAT'
 INTERPARK_SITE_CALENDAR='https://api-ticketfront.interpark.com/v1/goods/#INTERPARK_SITE_CODE#/playSeq?endDate=#END_DATE#&goodsCode=#INTERPARK_SITE_CODE#&page=1&pageSize=1550&preSale=false&startDate=#START_DATE#'
