657e184110bd9eef476784a99d8116fafc864bf0
devsunset <devsunset@gmail.com>
2023-06-19 18:13:00 +0900

    dev

--- common/config.py
+++ common/config.py
@@ -21,7 +21,7 @@ TELEGRAM_CHAT_ID =-1001646770464
 HOLYDAY='2023-05-05' 
 
 # skip day 
-SKIP_DAY='2023-06-17,2023-06-24,2023-07-01,2023-07-29,2023-08-12,2023-08-19,2023-09-09,2023-09-23,2023-11-11,2023-12-02' 
+SKIP_DAY='2023-06-24,2023-07-01,2023-07-22,2023-07-29,2023-08-12,2023-08-19,2023-09-09,2023-09-23,2023-11-11,2023-12-02' 
 
 # site  interpark (seperate comma)
 # INTERPARK_SITE_NAME='천왕산가족캠핑장,노을진(進)캠핑장,한탄강오토캠핑장,경기도청소년수련원캠핑장안산,공릉관광지가족캠핑장,고양시킨텍스캠핑장,연천재인폭포오토캠핑장,평택내리캠핑장,평택소풍정원캠핑장,평택진위천유원지캠핑장,안성맞춤캠핑장'
@@ -29,16 +29,10 @@ SKIP_DAY='2023-06-17,2023-06-24,2023-07-01,2023-07-29,2023-08-12,2023-08-19,2023
 # INTERPARK_SITE_CHECK_DAY='5,5,5,5,5,5,5,5,5,5,5'      # Friday (4) , Saturday (5),  Sunday (6)
 # INTERPARK_SITE_SEAT_GRADE='1,1:2,1,1:2,1,1,1,1,1:2,1,3'
 
-# INTERPARK_SITE_NAME='천왕산가족캠핑장,노을진(進)캠핑장,한탄강오토캠핑장,경기도청소년수련원캠핑장안산,고양시킨텍스캠핑장,연천재인폭포오토캠핑장'
-# INTERPARK_SITE_CODE='21012652,22011899,21005592,20003920,22005895,22016459'
-# INTERPARK_SITE_CHECK_DAY='5,5,5,5,5,5,'      # Friday (4) , Saturday (5),  Sunday (6)
-# INTERPARK_SITE_SEAT_GRADE='1,1:2,1,1:2,1,1'
-
-INTERPARK_SITE_NAME='천왕산가족캠핑장,노을진(進)캠핑장,한탄강오토캠핑장,고양시킨텍스캠핑장,연천재인폭포오토캠핑장'
-INTERPARK_SITE_CODE='21012652,22011899,21005592,22005895,22016459'
-INTERPARK_SITE_CHECK_DAY='5,5,5,5,5,'      # Friday (4) , Saturday (5),  Sunday (6)
-INTERPARK_SITE_SEAT_GRADE='1,1:2,1,1,1'
-
+INTERPARK_SITE_NAME='천왕산가족캠핑장,노을진(進)캠핑장,한탄강오토캠핑장,경기도청소년수련원캠핑장안산,고양시킨텍스캠핑장,연천재인폭포오토캠핑장'
+INTERPARK_SITE_CODE='21012652,22011899,21005592,20003920,22005895,22016459'
+INTERPARK_SITE_CHECK_DAY='5,5,5,5,5,5,'      # Friday (4) , Saturday (5),  Sunday (6)
+INTERPARK_SITE_SEAT_GRADE='1,1:2,1,1:2,1,1'
 
 
 INTERPARK_SITE_CHECK_URL='https://api-ticketfront.interpark.com/v1/goods/#INTERPARK_SITE_CODE#/playSeq/PlaySeq/#PLAYSEQ#/REMAINSEAT'
